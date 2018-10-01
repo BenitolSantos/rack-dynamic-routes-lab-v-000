b22a754e31d873d1f69d5c33ef6f46750d35e02a
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-10-01 23:52:37 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -13,9 +13,7 @@ class Application
       item_name = req.path.split(/items/).last #uses regex to split and remove the folder
       #ex; turn /songs/Sorry into Sorry
       item = @@items.find{|i| item.name == item_name}
-      resp.write song.artist
-
-
+      resp.write item.name
     end
 
     resp.finish
