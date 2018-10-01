4a8b64f7428077b33841363b43dedcfc6d50ed1c
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-10-01 23:48:07 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,9 +10,10 @@ class Application
     end
 
     if req.path.match(/items/)
+      item_name = req.path.split(/items/).last
+      item = @@items.find{|i| item.name == item_name}
 
-
-    resp.finish
-  end
+      resp.finish
+    end
   end
 end
