a6ba702b2e9554a76579d12acc9a0ec603e33d28
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-10-02 00:26:24 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last #uses regex to split and remove the folder
+      #remember that split needs to be on a string
       #ex; turn /songs/Sorry into Sorry
       item = @@items.find{|i| i.name == item_name}
       if item
