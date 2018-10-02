d9b9593cc67873696a883458dd16c24a5c488261
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-10-02 00:15:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,10 +5,6 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    @@items.each do |item|
-      resp.write "{item.name}\n"
-    end
-
     if req.path.match(/items/)
       item_name = req.path.split(/items/).last #uses regex to split and remove the folder
       #ex; turn /songs/Sorry into Sorry
