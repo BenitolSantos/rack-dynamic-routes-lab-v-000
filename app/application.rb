9a3243a1d862d935cc035d2c8012bbf3cca475d6
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-10-02 00:00:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       item = @@items.find{|i| item.name == item_name}
       resp.write item.price #price and name are the two attributes of the item class
       else
-        
+        resp.status = 400
       end
     else #returns 404 status code for a bad route
       resp.status = 404
